a85abe117c516cd3bf4e8e19acf512ca6129e3d4
dtrg <dtrg@localhost>
2011-10-05 23:42:50 +0000

    Add support for LuaJIT 2. Now use node.js for the Javascript interpreter rather than SpiderMonkey (as smjs is no longer in Debian).

--- src/javascript/run.js
+++ src/javascript/run.js
@@ -10,23 +10,48 @@
  * $LastChangedDate: 2007-04-30 22:41:42 +0000 (Mon, 30 Apr 2007) $
  */
 
+/* Some setup. */
+
+var fs = require("fs");
+var sys = require("sys");
+
+function print(s)
+{
+	sys.print(s);
+	sys.print("\n");
+}
+
+function quit()
+{
+	process.exit();
+}
+
+/* Node doesn't have an include() function. And eval() has to be called from
+ * the top level or it doesn't work. WTF? */
+
+function load(filename)
+{
+	return fs.readFileSync(filename, "utf-8");
+}
+
 /* Load the Clue crt and libc. */
 
-load("src/javascript/crt.js");
-load("src/javascript/libc.js");
+eval(load("src/javascript/crt.js"));
+eval(load("src/javascript/libc.js"));
 
 /* Load the Clue compiled program. */
 
-var argc = 1;
-while (arguments[argc] != "--")
+var argv = process.argv;
+var argc = 3;
+while (argv[argc] != "--")
 {
-	if (!arguments[argc])
+	if (!argv[argc])
 	{
-		print("Command line did not contain a --");
+		print("Command line did not contain a --\n");
 		quit();
 	}
-	
-	load(arguments[argc]);
+
+	eval(load(argv[argc]));
 	argc++;
 }
 
@@ -35,14 +60,14 @@ while (arguments[argc] != "--")
 {
 	var cargs = [];
 	
-	for (var i = argc+1; i < arguments.length; i++)
+	for (var i = argc+1; i < argv.length; i++)
 	{
-		var v = clue_newstring(arguments[i]);
+		var v = clue_newstring(argv[i]);
 		cargs.push(0);
 		cargs.push(v);
 	}
-	
+
     clue_run_initializers();
-    _main(0, [], arguments.length - argc, 0, cargs);
+    _main(0, [], argv.length - argc, 0, cargs);
     quit();
 }
